a8ed31757202c205df3c6df9d084df1cea2caeb2
RedLime <redlimerl@gmail.com>
2025-07-29 04:35:23 +0900

    Update: update speedrun major versions

--- src/main/kotlin/com/redlimerl/mcsrlauncher/util/SpeedrunUtils.kt
+++ src/main/kotlin/com/redlimerl/mcsrlauncher/util/SpeedrunUtils.kt
@@ -3,12 +3,14 @@ package com.redlimerl.mcsrlauncher.util
 object SpeedrunUtils {
     val MAJOR_SPEEDRUN_VERSIONS = listOf(
         "1.16.1",
-        "1.15.2",
-        "1.14.4",
-        "1.8.9",
-        "1.7.10",
         "1.16.5",
         "1.17.1",
-        "1.12.2"
+        "1.15.2",
+        "1.14.4",
+        "1.12.2",
+        "1.11.2",
+        "1.8.9",
+        "1.8",
+        "1.7.10"
     )
 }
